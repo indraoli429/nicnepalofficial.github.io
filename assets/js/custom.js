bfe64f68d4e7dbe5c67c0b72d280b55f60522442
Cool BRG <budhram.gurung01@gmail.com>
2019-07-31 20:21:19 +0545

    Home page banner done

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,11 @@
 /*Custom JavaScript*/
 
 $(function(){
-  
+  var typed = new Typed(".typed", {
+    strings: ["is with you.", "goal is to build economically developed Nepal."],
+    typeSpeed: 40,
+    startDelay: 500,
+    backSpeed: 50
+  });
 });
+
