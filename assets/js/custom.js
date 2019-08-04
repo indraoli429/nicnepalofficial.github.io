666d272a30701a8fd8cdf05ac9d153ba5c3f06e2
Cool BRG <budhram.gurung01@gmail.com>
2019-08-04 15:29:54 +0545

    Added elevator pitch and footer content improved

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,7 +2,7 @@
 
 $(function(){
   var typed = new Typed(".typed", {
-    strings: ["is with you.", "goal is to build economically developed Nepal."],
+    strings: ["is with you.", "goal is to develop Nepal economically."],
     typeSpeed: 40,
     startDelay: 500,
     backSpeed: 50
